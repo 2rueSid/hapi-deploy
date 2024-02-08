21a7509614883c8d67f6e23856aba3097b5b872c
Dima Kachurovskyy <mekaa2.718@gmail.com>
2024-02-08 16:25:07 -0500

    vpc update

--- main.tf
+++ main.tf
@@ -3,29 +3,39 @@ provider "aws" {
 }
 
 locals {
-  azs = formatlist("${var.region}%s", ["a", "b"])
+  azs  = formatlist("${var.region}%s", ["a", "b"])
+  cidr = "10.0.0.0/16"
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name    = var.name
-  version = "5.5.1"
-  cidr    = "10.0.0.0/16"
+  name            = var.name
+  version         = "5.5.1"
+  cidr            = local.cidr
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
 
-  azs                     = local.azs
-  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   map_public_ip_on_launch = true
-
-  enable_nat_gateway   = true
-  create_igw           = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway      = true
+  create_igw              = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
 
   tags = {
     Hapi = true
     Test = true
   }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.2.0"
+
+
 }
